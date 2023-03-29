3e72473be064212b49c99c804f623fcb1ea59129
Pedro Martins <rechousa@gmail.com>
2023-03-30 00:43:41 +0100

    Add Multiple questions Add Previous/next buttons

--- app/src/main/java/com/rechousa/geoquiz/MainActivity.kt
+++ app/src/main/java/com/rechousa/geoquiz/MainActivity.kt
@@ -2,36 +2,69 @@ package com.rechousa.geoquiz
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.rechousa.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+
+    private var questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        binding.questionTextView.setOnClickListener { nextQuestion() }
 
-        trueButton.setOnClickListener { view: View ->
-            Snackbar.make(
-                trueButton,
-                R.string.correct_toast,
-                Snackbar.LENGTH_SHORT,
-            ).show()
+        binding.trueButton.setOnClickListener { view: View -> checkAnswer(view, true) }
+
+        binding.falseButton.setOnClickListener { view: View -> checkAnswer(view, false) }
+
+        binding.previousButton.setOnClickListener { previousQuestion() }
+
+        binding.nextButton.setOnClickListener { nextQuestion() }
+
+        updateQuestion()
+    }
+
+    private fun previousQuestion() {
+        currentIndex = (currentIndex + questionBank.size - 1) % questionBank.size
+        updateQuestion()
+    }
+
+    private fun nextQuestion() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResourceId = questionBank[currentIndex].textResourceId
+        binding.questionTextView.setText(questionTextResourceId)
+    }
+
+    private fun checkAnswer(view: View, userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResourceId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
         }
-
-        falseButton.setOnClickListener { view: View ->
-            Snackbar.make(
-                falseButton,
-                R.string.incorrect_toast,
-                Snackbar.LENGTH_SHORT,
-            ).show()
-        }
+        Snackbar.make(
+            view,
+            messageResourceId,
+            Snackbar.LENGTH_SHORT,
+        ).show()
     }
 }
---
+++ app/src/main/java/com/rechousa/geoquiz/Question.kt
@@ -0,0 +1,5 @@
+package com.rechousa.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResourceId: Int, val answer: Boolean)
